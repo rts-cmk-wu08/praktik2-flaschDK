1ebde2e6c3c2bf27de087faecbc2200d06e6c404
Flemming Rasch <90891617+flaschDK@users.noreply.github.com>
2023-05-26 14:12:31 +0200

    home.jsx data

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,6 +11,7 @@ const Home = () => {
     const homes = useLoaderData()
     const homesCount = useContext(UserContext).homesCount
     console.log(homes)
+    console.log(agents)
     
     return ( 
         <>
@@ -22,7 +23,27 @@ const Home = () => {
         <ul>
         {agents.map((agent) => <li key={agent.id}><Link to={"/agentdetails/"+agent.id}>{agent.id}</Link>  </li>)}
         </ul>
-        {homes.map((home) => <img key={home.id} src={home.images[0].url}/> )}
+        {homes.map((home) => 
+        <article key={home.id}>
+     <img key={home.id} src={home.images[0].url}/>
+     <p>{home.adress1}</p>
+     <p>{home.city}</p>
+     <p>{home.type}</p>
+     <p>{home.netto}</p>
+     <p>{home.energylabel}</p>
+     <p>{home.rooms}</p>
+     <p>{home.livingspace}</p>
+     <p>{home.price}</p>
+     </article>
+        )}
+         {agents.map((agent) => 
+  <article key={agent.id}>
+     <img key={agent.id} src={agent.image.url}/>
+     <p>{agent.name}</p>
+     <p>{agent.title}</p>
+     <p>{agent.email}</p>
+     </article>
+         )}
         </HomeStyle>
         </>
      );
