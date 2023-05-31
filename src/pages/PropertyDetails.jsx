ff00fdd86b072db84151fb042a6fe3de4f0cc5e4
Flemming Rasch <90891617+flaschDK@users.noreply.github.com>
2023-05-31 13:48:05 +0200

    flere detaljer

--- src/pages/PropertyDetails.jsx
+++ src/pages/PropertyDetails.jsx
@@ -7,28 +7,43 @@ const PropertyDetails = () => {
   return (
     <>
       <p>Gad vide hvem der bor her?</p>
-      <article key={home.id}>
+      <section key={home.id}>
         <img key={home.id} src={home.images[0].url} />
+        <article>
         <p>{home.adress1}</p>
         <p>{home.postalcode} {" "} {home.city}</p>
         <p>{home.price}</p>
-        <p>
+        </article>
+       <article>
+       <p>
           {Math.floor(Math.cos(home.long) * Math.cos(home.lat) * 10000000000)}
         </p>
         <p>{home.livingspace}</p>
         <p>{home.lotsize}</p>
         <p>{home.rooms}</p>
         <p>1</p>
-        <p>{home.basement}</p>
+       </article>
+       <article>
+       <p>{home.basement}</p>
         <p>{home.built}</p>
         <p>{home.remodel}</p>
         <p>{home.energylabel}</p>
-        <p>{home.payment}</p>
+       </article>
+       <article>
+       <p>{home.payment}</p>
         <p>{home.gross}</p>
         <p>{home.netto}</p>
         <p>{home.cost}</p>
-      </article>
-      <Agent agent={home.agent}/>
+       </article>
+       <article>
+        <h3>Beskrivelse</h3>
+       <p>{home.description}</p>
+       </article>
+       <article>
+        <h3>Ansvarlig mægler</h3>
+       <Agent agent={home.agent}/>
+       </article>
+      </section>
       <Outlet />
     </>
   );
