ccf65c5c4a91075493d37da761d979738a0bfdb3
Flemming Rasch <90891617+flaschDK@users.noreply.github.com>
2023-06-01 13:58:57 +0200

    sat nogle React icons på

--- src/pages/PropertyDetails.jsx
+++ src/pages/PropertyDetails.jsx
@@ -1,5 +1,7 @@
 import { useLoaderData, Outlet } from "react-router-dom";
-import Agent from "../components/Agent"
+import { RiHeartLine, RiImageLine } from "react-icons/ri";
+import { IoLocationOutline, IoLayersOutline } from "react-icons/io5";
+import Agent from "../components/Agent";
 
 const PropertyDetails = () => {
   const home = useLoaderData();
@@ -10,39 +12,51 @@ const PropertyDetails = () => {
       <section key={home.id}>
         <img key={home.id} src={home.images[0].url} />
         <article>
-        <p>{home.adress1}</p>
-        <p>{home.postalcode} {" "} {home.city}</p>
-        <p>{home.price}</p>
+          <div>
+            <p>{home.adress1}</p>
+            <p>
+              {home.postalcode} {home.city}
+            </p>
+          </div>
+          <div>
+            <RiImageLine />
+            <IoLocationOutline />
+            <IoLayersOutline />
+            <RiHeartLine />
+          </div>
+          <div>
+            <p>{home.price}</p>
+          </div>
         </article>
-       <article>
-       <p>
-          {Math.floor(Math.cos(home.long) * Math.cos(home.lat) * 10000000000)}
-        </p>
-        <p>{home.livingspace}</p>
-        <p>{home.lotsize}</p>
-        <p>{home.rooms}</p>
-        <p>1</p>
-       </article>
-       <article>
-       <p>{home.basement}</p>
-        <p>{home.built}</p>
-        <p>{home.remodel}</p>
-        <p>{home.energylabel}</p>
-       </article>
-       <article>
-       <p>{home.payment}</p>
-        <p>{home.gross}</p>
-        <p>{home.netto}</p>
-        <p>{home.cost}</p>
-       </article>
-       <article>
-        <h3>Beskrivelse</h3>
-       <p>{home.description}</p>
-       </article>
-       <article>
-        <h3>Ansvarlig mægler</h3>
-       <Agent agent={home.agent}/>
-       </article>
+        <article>
+          <p>
+            {Math.floor(Math.cos(home.long) * Math.cos(home.lat) * 10000000000)}
+          </p>
+          <p>{home.livingspace}</p>
+          <p>{home.lotsize}</p>
+          <p>{home.rooms}</p>
+          <p>1</p>
+        </article>
+        <article>
+          <p>{home.basement}</p>
+          <p>{home.built}</p>
+          <p>{home.remodel}</p>
+          <p>{home.energylabel}</p>
+        </article>
+        <article>
+          <p>{home.payment}</p>
+          <p>{home.gross}</p>
+          <p>{home.netto}</p>
+          <p>{home.cost}</p>
+        </article>
+        <article>
+          <h3>Beskrivelse</h3>
+          <p>{home.description}</p>
+        </article>
+        <article>
+          <h3>Ansvarlig mægler</h3>
+          <Agent agent={home.agent} />
+        </article>
       </section>
       <Outlet />
     </>
